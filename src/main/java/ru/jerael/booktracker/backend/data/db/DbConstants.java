ec1b4449b13ee05c6962f37224175723dcc597d2
Jerael <l.kroyan43@gmail.com>
2026-02-14 21:17:50 +0300

    feat: add genre entity and jpa repository definition

---
+++ src/main/java/ru/jerael/booktracker/backend/data/db/DbConstants.java
@@ -0,0 +1,8 @@
+package ru.jerael.booktracker.backend.data.db;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DbConstants {
+    public final String TABLE_GENRES = "genres";
+}
