9ef78eeaa283b8e61b1aa5f9242aa1be49b831ad
Jerael <l.kroyan43@gmail.com>
2026-02-15 17:12:43 +0300

    feat: add slf4j logger for general exception

--- src/main/java/ru/jerael/booktracker/backend/api/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/jerael/booktracker/backend/api/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.jerael.booktracker.backend.api.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,7 @@ import ru.jerael.booktracker.backend.domain.exception.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
@@ -46,6 +49,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        logger.error("Unhandled exception occurred", ex);
         ErrorResponse errorResponse = new ErrorResponse(
                 "INTERNAL_SERVER_ERROR",
                 "An internal server error occurred"
