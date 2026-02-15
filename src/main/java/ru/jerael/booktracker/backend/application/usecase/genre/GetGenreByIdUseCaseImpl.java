711d27e8371fc6ccb4b4d4ede292c082dbc4acda
Jerael <l.kroyan43@gmail.com>
2026-02-15 16:57:58 +0300

    feat: add error handling to genres

--- src/main/java/ru/jerael/booktracker/backend/application/usecase/genre/GetGenreByIdUseCaseImpl.java
+++ src/main/java/ru/jerael/booktracker/backend/application/usecase/genre/GetGenreByIdUseCaseImpl.java
@@ -1,9 +1,9 @@
 package ru.jerael.booktracker.backend.application.usecase.genre;
 
+import ru.jerael.booktracker.backend.domain.exception.NotFoundException;
 import ru.jerael.booktracker.backend.domain.model.Genre;
 import ru.jerael.booktracker.backend.domain.repository.GenreRepository;
 import ru.jerael.booktracker.backend.domain.usecase.genre.GetGenreByIdUseCase;
-import java.util.Optional;
 
 public class GetGenreByIdUseCaseImpl implements GetGenreByIdUseCase {
     private final GenreRepository genreRepository;
@@ -13,7 +13,7 @@ public class GetGenreByIdUseCaseImpl implements GetGenreByIdUseCase {
     }
 
     @Override
-    public Optional<Genre> execute(Integer id) {
-        return genreRepository.getGenreById(id);
+    public Genre execute(Integer id) {
+        return genreRepository.getGenreById(id).orElseThrow(() -> NotFoundException.genreNotFound(id));
     }
 }
--- src/main/java/ru/jerael/booktracker/backend/domain/usecase/genre/GetGenreByIdUseCase.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/usecase/genre/GetGenreByIdUseCase.java
@@ -1,8 +1,7 @@
 package ru.jerael.booktracker.backend.domain.usecase.genre;
 
 import ru.jerael.booktracker.backend.domain.model.Genre;
-import java.util.Optional;
 
 public interface GetGenreByIdUseCase {
-    Optional<Genre> execute(Integer id);
+    Genre execute(Integer id);
 }
