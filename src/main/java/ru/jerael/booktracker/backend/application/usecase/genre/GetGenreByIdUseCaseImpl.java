45f5c9b290479ef7a9e224d0d5cb2e000a749f7b
Jerael <l.kroyan43@gmail.com>
2026-02-16 16:13:40 +0300

    refactor: add @Service annotation to genre use cases

--- src/main/java/ru/jerael/booktracker/backend/application/usecase/genre/GetGenreByIdUseCaseImpl.java
+++ src/main/java/ru/jerael/booktracker/backend/application/usecase/genre/GetGenreByIdUseCaseImpl.java
@@ -1,10 +1,12 @@
 package ru.jerael.booktracker.backend.application.usecase.genre;
 
+import org.springframework.stereotype.Service;
 import ru.jerael.booktracker.backend.domain.exception.NotFoundException;
 import ru.jerael.booktracker.backend.domain.model.Genre;
 import ru.jerael.booktracker.backend.domain.repository.GenreRepository;
 import ru.jerael.booktracker.backend.domain.usecase.genre.GetGenreByIdUseCase;
 
+@Service
 public class GetGenreByIdUseCaseImpl implements GetGenreByIdUseCase {
     private final GenreRepository genreRepository;
 
