902dce6710ce81d5f73588c3c8d100f2b6da6b45
Jerael <l.kroyan43@gmail.com>
2026-02-19 23:11:13 +0300

    feat: add file validator

---
+++ src/main/java/ru/jerael/booktracker/backend/api/validator/FileValidator.java
@@ -0,0 +1,18 @@
+package ru.jerael.booktracker.backend.api.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import ru.jerael.booktracker.backend.domain.exception.ValidationException;
+
+@Component
+public class FileValidator {
+    public void validate(MultipartFile file, String fieldName) {
+        if (file == null || file.isEmpty()) {
+            throw ValidationException.emptyFileContent(fieldName);
+        }
+        String fileName = file.getOriginalFilename();
+        if (fileName == null || fileName.isBlank()) {
+            throw ValidationException.emptyFileName(fieldName);
+        }
+    }
+}
