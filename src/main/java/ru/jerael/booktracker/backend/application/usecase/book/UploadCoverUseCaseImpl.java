6cf0538edf4a3d5e0209653d5d3344edfbfc2e62
Jerael <l.kroyan43@gmail.com>
2026-02-19 13:42:26 +0300

    feat: add CreateBookUseCaseImpl and UploadCoverUseCaseImpl

---
+++ src/main/java/ru/jerael/booktracker/backend/application/usecase/book/UploadCoverUseCaseImpl.java
@@ -0,0 +1,32 @@
+package ru.jerael.booktracker.backend.application.usecase.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.jerael.booktracker.backend.domain.constants.BookRules;
+import ru.jerael.booktracker.backend.domain.exception.NotFoundException;
+import ru.jerael.booktracker.backend.domain.exception.ValidationException;
+import ru.jerael.booktracker.backend.domain.model.book.Book;
+import ru.jerael.booktracker.backend.domain.model.book.UploadCover;
+import ru.jerael.booktracker.backend.domain.repository.BookRepository;
+import ru.jerael.booktracker.backend.domain.storage.BookCoverStorage;
+import ru.jerael.booktracker.backend.domain.usecase.book.UploadCoverUseCase;
+
+@Service
+@RequiredArgsConstructor
+public class UploadCoverUseCaseImpl implements UploadCoverUseCase {
+    private final BookRepository bookRepository;
+    private final BookCoverStorage bookCoverStorage;
+
+    @Override
+    @Transactional
+    public Book execute(UploadCover data) {
+        bookRepository.getBookById(data.bookId()).orElseThrow(() -> NotFoundException.bookNotFound(data.bookId()));
+        if (!BookRules.ALLOWED_IMAGE_MIME_TYPES.contains(data.contentType())) {
+            throw ValidationException.unsupportedFileContentType(data.contentType());
+        }
+        String extension = BookRules.MIME_TO_EXTENSION.get(data.contentType());
+        String path = bookCoverStorage.save(data.bookId(), extension, data.content());
+        return bookRepository.updateCoverUrl(data.bookId(), path);
+    }
+}
