000f5d3942e34ebaf9f17b7f6082b4cb24f120d0
Jerael <l.kroyan43@gmail.com>
2026-02-19 23:12:42 +0300

    feat: add upload cover endpoint and api mapper

--- src/main/java/ru/jerael/booktracker/backend/api/controller/BookController.java
+++ src/main/java/ru/jerael/booktracker/backend/api/controller/BookController.java
@@ -4,14 +4,19 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.jerael.booktracker.backend.api.dto.book.BookCreationRequest;
 import ru.jerael.booktracker.backend.api.dto.book.BookResponse;
 import ru.jerael.booktracker.backend.api.mapper.BookApiMapper;
+import ru.jerael.booktracker.backend.api.mapper.UploadCoverApiMapper;
+import ru.jerael.booktracker.backend.api.validator.FileValidator;
 import ru.jerael.booktracker.backend.domain.model.book.Book;
 import ru.jerael.booktracker.backend.domain.model.book.BookCreation;
+import ru.jerael.booktracker.backend.domain.model.book.UploadCover;
 import ru.jerael.booktracker.backend.domain.usecase.book.CreateBookUseCase;
 import ru.jerael.booktracker.backend.domain.usecase.book.GetBookByIdUseCase;
 import ru.jerael.booktracker.backend.domain.usecase.book.GetBooksUseCase;
+import ru.jerael.booktracker.backend.domain.usecase.book.UploadCoverUseCase;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,7 +27,10 @@ public class BookController {
     private final GetBooksUseCase getBooksUseCase;
     private final GetBookByIdUseCase getBookByIdUseCase;
     private final CreateBookUseCase createBookUseCase;
+    private final UploadCoverUseCase uploadCoverUseCase;
+    private final FileValidator fileValidator;
     private final BookApiMapper bookApiMapper;
+    private final UploadCoverApiMapper uploadCoverApiMapper;
 
     @GetMapping
     public List<BookResponse> getAll() {
@@ -41,6 +49,14 @@ public class BookController {
     public BookResponse create(@Valid @RequestBody BookCreationRequest request) {
         BookCreation data = bookApiMapper.toDomain(request);
         Book book = createBookUseCase.execute(data);
+        return bookApiMapper.toResponse(book);
+    }
+
+    @PatchMapping("/{id}/cover")
+    public BookResponse uploadCover(@PathVariable UUID id, @RequestParam("cover") MultipartFile file) {
+        fileValidator.validate(file, "cover");
+        UploadCover data = uploadCoverApiMapper.toDomain(id, file);
+        Book book = uploadCoverUseCase.execute(data);
         return bookApiMapper.toResponse(book);
     }
 }
