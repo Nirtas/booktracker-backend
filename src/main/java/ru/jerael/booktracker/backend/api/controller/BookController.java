327ef1bea905549767aff2269bcb13b3011ab43d
Jerael <l.kroyan43@gmail.com>
2026-02-19 19:26:17 +0300

    feat: add book creation endpoint

--- src/main/java/ru/jerael/booktracker/backend/api/controller/BookController.java
+++ src/main/java/ru/jerael/booktracker/backend/api/controller/BookController.java
@@ -1,13 +1,15 @@
 package ru.jerael.booktracker.backend.api.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.jerael.booktracker.backend.api.dto.book.BookCreationRequest;
 import ru.jerael.booktracker.backend.api.dto.book.BookResponse;
 import ru.jerael.booktracker.backend.api.mapper.BookApiMapper;
 import ru.jerael.booktracker.backend.domain.model.book.Book;
+import ru.jerael.booktracker.backend.domain.model.book.BookCreation;
+import ru.jerael.booktracker.backend.domain.usecase.book.CreateBookUseCase;
 import ru.jerael.booktracker.backend.domain.usecase.book.GetBookByIdUseCase;
 import ru.jerael.booktracker.backend.domain.usecase.book.GetBooksUseCase;
 import java.util.List;
@@ -19,6 +21,7 @@ import java.util.UUID;
 public class BookController {
     private final GetBooksUseCase getBooksUseCase;
     private final GetBookByIdUseCase getBookByIdUseCase;
+    private final CreateBookUseCase createBookUseCase;
     private final BookApiMapper bookApiMapper;
 
     @GetMapping
@@ -30,6 +33,14 @@ public class BookController {
     @GetMapping("/{id}")
     public BookResponse getById(@PathVariable UUID id) {
         Book book = getBookByIdUseCase.execute(id);
+        return bookApiMapper.toResponse(book);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookResponse create(@Valid @RequestBody BookCreationRequest request) {
+        BookCreation data = bookApiMapper.toDomain(request);
+        Book book = createBookUseCase.execute(data);
         return bookApiMapper.toResponse(book);
     }
 }
