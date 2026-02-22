05cc0859a592954ab5249fb0a537bb37124476da
Jerael <l.kroyan43@gmail.com>
2026-02-22 19:48:31 +0300

    refactor: remove unused validation errors

--- src/main/java/ru/jerael/booktracker/backend/domain/exception/ValidationException.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/exception/ValidationException.java
@@ -20,22 +20,6 @@ public class ValidationException extends AppException {
         );
     }
 
-    public static ValidationException fieldCannotBeEmpty(String fieldName) {
-        return new ValidationException(
-            fieldName + " cannot be empty",
-            "FIELD_CANNOT_BE_EMPTY",
-            fieldName
-        );
-    }
-
-    public static ValidationException fieldTooLong(String fieldName, int maxLength) {
-        return new ValidationException(
-            fieldName + " is too long (max " + maxLength + ")",
-            "FIELD_TOO_LONG",
-            fieldName
-        );
-    }
-
     public static ValidationException emptyFileName(String fieldName) {
         return new ValidationException(
             "File name cannot be empty",
