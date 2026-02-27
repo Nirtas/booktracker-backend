4a466ec9b2bcb53909d2e9943ef913f96685d3e4
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-02-27 04:23:32 +0100

    feat: implement dedicated endpoint for cover deletion (#27)
    * feat: implement book cover deletion
    
    * test: add tests for book cover deletion

---
+++ src/main/java/ru/jerael/booktracker/backend/application/usecase/book/DeleteCoverUseCaseImpl.java
@@ -0,0 +1,37 @@
+package ru.jerael.booktracker.backend.application.usecase.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.jerael.booktracker.backend.domain.exception.factory.BookExceptionFactory;
+import ru.jerael.booktracker.backend.domain.model.book.Book;
+import ru.jerael.booktracker.backend.domain.repository.BookRepository;
+import ru.jerael.booktracker.backend.domain.storage.BookCoverStorage;
+import ru.jerael.booktracker.backend.domain.usecase.book.DeleteCoverUseCase;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteCoverUseCaseImpl implements DeleteCoverUseCase {
+    private final BookRepository bookRepository;
+    private final BookCoverStorage bookCoverStorage;
+
+    @Override
+    @Transactional
+    public void execute(UUID bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> BookExceptionFactory.notFound(bookId));
+        if (book.coverUrl() == null) return;
+
+        Book updatedBook = new Book(
+            book.id(),
+            book.title(),
+            book.author(),
+            null,
+            book.status(),
+            book.createdAt(),
+            book.genres()
+        );
+        bookRepository.save(updatedBook);
+        bookCoverStorage.delete(book.coverUrl());
+    }
+}
