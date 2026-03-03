b0be9929dfee8e9f6bdd613cd0ba49d57b2ec1f3
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-03-03 19:50:44 +0100

    feat: integrate swagger (#54)
    * build: add springdoc-openapi-starter-webmvc-ui dependency
    
    * chore: add swagger props in application.yaml and .env.example
    
    * feat: integrate swagger documentation

--- src/main/java/ru/jerael/booktracker/backend/web/controller/GenreController.java
+++ src/main/java/ru/jerael/booktracker/backend/web/controller/GenreController.java
@@ -1,17 +1,20 @@
 package ru.jerael.booktracker.backend.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.jerael.booktracker.backend.web.dto.genre.GenreResponse;
-import ru.jerael.booktracker.backend.web.mapper.GenreWebMapper;
 import ru.jerael.booktracker.backend.domain.model.Genre;
 import ru.jerael.booktracker.backend.domain.usecase.genre.GetGenreByIdUseCase;
 import ru.jerael.booktracker.backend.domain.usecase.genre.GetGenresUseCase;
+import ru.jerael.booktracker.backend.web.dto.genre.GenreResponse;
+import ru.jerael.booktracker.backend.web.mapper.GenreWebMapper;
 import java.util.Set;
 
+@Tag(name = "Genres")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/genres")
@@ -20,12 +23,14 @@ public class GenreController {
     private final GetGenreByIdUseCase getGenreByIdUseCase;
     private final GenreWebMapper genreWebMapper;
 
+    @Operation(summary = "Get all genres")
     @GetMapping
     public Set<GenreResponse> getAll() {
         Set<Genre> genres = getGenresUseCase.execute();
         return genreWebMapper.toResponses(genres);
     }
 
+    @Operation(summary = "Get genre by id")
     @GetMapping("/{id}")
     public GenreResponse getById(@PathVariable Integer id) {
         Genre genre = getGenreByIdUseCase.execute(id);
--- src/main/java/ru/jerael/booktracker/backend/web/dto/book/BookDetailsUpdateRequest.java
+++ src/main/java/ru/jerael/booktracker/backend/web/dto/book/BookDetailsUpdateRequest.java
@@ -1,5 +1,6 @@
 package ru.jerael.booktracker.backend.web.dto.book;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,16 +10,19 @@ import java.util.Set;
 
 public record BookDetailsUpdateRequest(
     @Nullable
+    @Schema(example = "Book Title")
     @Pattern(regexp = ValidationRegex.PATTERN_NOT_BLANK, message = "must not be blank")
     @Size(max = BookRules.TITLE_MAX_LENGTH)
     String title,
 
     @Nullable
+    @Schema(example = "Book Author")
     @Pattern(regexp = ValidationRegex.PATTERN_NOT_BLANK, message = "must not be blank")
     @Size(max = BookRules.AUTHOR_MAX_LENGTH)
     String author,
 
     @Nullable
+    @Schema(example = "READING")
     String status,
 
     @Nullable
