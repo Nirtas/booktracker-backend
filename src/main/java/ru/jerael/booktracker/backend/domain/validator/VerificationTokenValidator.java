9eddf357ec0f6c86d3f685e38a1228c72d808d99
Jerael <l.kroyan43@gmail.com>
2026-03-08 22:09:06 +0300

    refactor: rename code to token in VerificationTokenValidator

--- src/main/java/ru/jerael/booktracker/backend/domain/validator/VerificationTokenValidator.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/validator/VerificationTokenValidator.java
@@ -3,5 +3,5 @@ package ru.jerael.booktracker.backend.domain.validator;
 import ru.jerael.booktracker.backend.domain.model.verification.EmailVerification;
 
 public interface VerificationTokenValidator {
-    void validate(EmailVerification emailVerification, String code);
+    void validate(EmailVerification emailVerification, String token);
 }
