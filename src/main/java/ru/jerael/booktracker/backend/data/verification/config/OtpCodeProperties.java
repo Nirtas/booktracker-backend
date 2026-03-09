ffbc270d9eb3b14e8bfa61ca8d381a1e7b00691b
Jerael <l.kroyan43@gmail.com>
2026-03-10 01:52:43 +0300

    feat: add EmailVerificationRules to OtpCodeProperties

--- src/main/java/ru/jerael/booktracker/backend/data/verification/config/OtpCodeProperties.java
+++ src/main/java/ru/jerael/booktracker/backend/data/verification/config/OtpCodeProperties.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+import ru.jerael.booktracker.backend.domain.constant.EmailVerificationRules;
 import java.time.Duration;
 
 @Component
@@ -13,8 +14,8 @@ import java.time.Duration;
 @ConfigurationProperties(prefix = "app.otp")
 @Validated
 public class OtpCodeProperties {
-    @Min(1)
-    @Max(64)
+    @Min(EmailVerificationRules.TOKEN_MIN_LENGTH)
+    @Max(EmailVerificationRules.TOKEN_MAX_LENGTH)
     private int length = 6;
 
     private Duration expiry = Duration.ofMinutes(10);
