5176893ba6cc7971e256de58d069347a16eb4cf3
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-03-09 15:15:01 +0100

    feat: add refresh tokens table and repository (#80)
    * feat: add migration for refresh_tokens table
    
    * feat: add RefreshTokenEntity
    
    * feat: add RefreshToken model and data mapper
    
    * feat: add RefreshTokenRepository
    
    * test: add tests for refresh token data mapper and repository

---
+++ src/main/java/ru/jerael/booktracker/backend/data/db/repository/JpaRefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package ru.jerael.booktracker.backend.data.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.jerael.booktracker.backend.data.db.entity.RefreshTokenEntity;
+import java.util.UUID;
+
+@Repository
+public interface JpaRefreshTokenRepository extends JpaRepository<RefreshTokenEntity, UUID> {}
