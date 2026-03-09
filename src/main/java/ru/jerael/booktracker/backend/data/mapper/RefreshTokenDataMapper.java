0f337d5afdae7ea2f4014c3a88169ecc85a47a03
Jerael <l.kroyan43@gmail.com>
2026-03-09 16:56:19 +0300

    feat: add RefreshToken model and data mapper

---
+++ src/main/java/ru/jerael/booktracker/backend/data/mapper/RefreshTokenDataMapper.java
@@ -0,0 +1,19 @@
+package ru.jerael.booktracker.backend.data.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.jerael.booktracker.backend.data.db.entity.RefreshTokenEntity;
+import ru.jerael.booktracker.backend.domain.model.auth.RefreshToken;
+
+@Component
+public class RefreshTokenDataMapper {
+    public RefreshTokenEntity toEntity(RefreshToken refreshToken) {
+        if (refreshToken == null) return null;
+
+        RefreshTokenEntity entity = new RefreshTokenEntity();
+        entity.setId(refreshToken.id());
+        entity.setUserId(refreshToken.userId());
+        entity.setTokenHash(refreshToken.tokenHash());
+        entity.setExpiresAt(refreshToken.expiresAt());
+        return entity;
+    }
+}
