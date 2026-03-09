4baf18b6564e9a776d9960f3c0b23a8989dc4ffb
Jerael <l.kroyan43@gmail.com>
2026-03-10 00:27:42 +0300

    feat: add AuthTokenService

---
+++ src/main/java/ru/jerael/booktracker/backend/application/service/AuthTokenServiceImpl.java
@@ -0,0 +1,33 @@
+package ru.jerael.booktracker.backend.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.jerael.booktracker.backend.domain.hasher.PasswordHasher;
+import ru.jerael.booktracker.backend.domain.model.auth.GeneratedToken;
+import ru.jerael.booktracker.backend.domain.model.auth.RefreshToken;
+import ru.jerael.booktracker.backend.domain.model.auth.TokenPair;
+import ru.jerael.booktracker.backend.domain.repository.RefreshTokenRepository;
+import ru.jerael.booktracker.backend.domain.service.token.AuthTokenService;
+import ru.jerael.booktracker.backend.domain.service.token.IdentityTokenProvider;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuthTokenServiceImpl implements AuthTokenService {
+    private final IdentityTokenProvider identityTokenProvider;
+    private final PasswordHasher passwordHasher;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    @Transactional
+    public TokenPair issueTokens(UUID userId) {
+        Map<String, Object> claims = Map.of("sub", userId);
+        String accessToken = identityTokenProvider.generateAccessToken(claims);
+        GeneratedToken refreshToken = identityTokenProvider.generateRefreshToken();
+        String hash = passwordHasher.hash(refreshToken.value());
+        refreshTokenRepository.save(new RefreshToken(null, userId, hash, refreshToken.expiresAt()));
+        return new TokenPair(accessToken, refreshToken.value());
+    }
+}
