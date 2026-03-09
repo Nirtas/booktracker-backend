b0805daaf4e2fce63e10ef502f87dacf1577f986
Jerael <l.kroyan43@gmail.com>
2026-03-10 01:27:57 +0300

    feat: add userNotFound and userAlreadyVerified exceptions

--- src/main/java/ru/jerael/booktracker/backend/domain/exception/factory/UserExceptionFactory.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/exception/factory/UserExceptionFactory.java
@@ -1,7 +1,13 @@
 package ru.jerael.booktracker.backend.domain.exception.factory;
 
 import ru.jerael.booktracker.backend.domain.exception.AlreadyExistsException;
+import ru.jerael.booktracker.backend.domain.exception.NotFoundException;
+import ru.jerael.booktracker.backend.domain.exception.ValidationException;
 import ru.jerael.booktracker.backend.domain.exception.code.UserErrorCode;
+import ru.jerael.booktracker.backend.domain.exception.model.ValidationError;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class UserExceptionFactory {
     public static AlreadyExistsException emailAlreadyExists(String email) {
@@ -9,5 +15,23 @@ public class UserExceptionFactory {
             UserErrorCode.EMAIL_ALREADY_EXISTS,
             "User with email " + email + " already exists"
         );
+    }
+
+    public static NotFoundException userNotFound(UUID id) {
+        return new NotFoundException(
+            UserErrorCode.USER_NOT_FOUND,
+            "User with id " + id + " was not found"
+        );
+    }
+
+    public static ValidationException userAlreadyVerified(UUID id) {
+        return new ValidationException(List.of(
+            new ValidationError(
+                UserErrorCode.ALREADY_VERIFIED.name(),
+                "userId",
+                "User with id " + id + " already verified",
+                Map.of()
+            )
+        ));
     }
 }
