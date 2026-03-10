fcd42062afecf90845ed7995aa7cab9699ba3fcc
Jerael <l.kroyan43@gmail.com>
2026-03-10 10:39:06 +0300

    feat: add confirm-registration endpoint

--- src/main/java/ru/jerael/booktracker/backend/web/controller/AuthController.java
+++ src/main/java/ru/jerael/booktracker/backend/web/controller/AuthController.java
@@ -6,11 +6,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.jerael.booktracker.backend.domain.model.auth.ConfirmRegistration;
+import ru.jerael.booktracker.backend.domain.model.auth.TokenPair;
 import ru.jerael.booktracker.backend.domain.model.user.UserCreation;
 import ru.jerael.booktracker.backend.domain.model.user.UserCreationResult;
+import ru.jerael.booktracker.backend.domain.usecase.auth.ConfirmRegistrationUseCase;
 import ru.jerael.booktracker.backend.domain.usecase.user.CreateUserUseCase;
+import ru.jerael.booktracker.backend.web.dto.auth.AuthResponse;
+import ru.jerael.booktracker.backend.web.dto.auth.ConfirmRegistrationRequest;
 import ru.jerael.booktracker.backend.web.dto.user.UserCreationRequest;
 import ru.jerael.booktracker.backend.web.dto.user.UserCreationResponse;
+import ru.jerael.booktracker.backend.web.mapper.AuthWebMapper;
 import ru.jerael.booktracker.backend.web.mapper.UserWebMapper;
 
 @Tag(name = "Auth")
@@ -19,7 +25,9 @@ import ru.jerael.booktracker.backend.web.mapper.UserWebMapper;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final UserWebMapper userWebMapper;
+    private final AuthWebMapper authWebMapper;
     private final CreateUserUseCase createUserUseCase;
+    private final ConfirmRegistrationUseCase confirmRegistrationUseCase;
 
     @Operation(summary = "Register new user")
     @PostMapping("/register")
@@ -28,5 +36,13 @@ public class AuthController {
         UserCreation data = userWebMapper.toDomain(request);
         UserCreationResult result = createUserUseCase.execute(data);
         return userWebMapper.toResponse(result);
+    }
+
+    @Operation(summary = "Confirm registration")
+    @PostMapping("/confirm-registration")
+    public AuthResponse confirmRegistration(@Valid @RequestBody ConfirmRegistrationRequest request) {
+        ConfirmRegistration data = authWebMapper.toDomain(request);
+        TokenPair tokenPair = confirmRegistrationUseCase.execute(data);
+        return authWebMapper.toResponse(tokenPair);
     }
 }
