10df49e80f4bae2edc69a9448feee4fee529085c
Jerael <l.kroyan43@gmail.com>
2026-03-12 19:30:17 +0300

    feat: enable async for smtp

--- src/main/java/ru/jerael/booktracker/backend/Application.java
+++ src/main/java/ru/jerael/booktracker/backend/Application.java
@@ -3,11 +3,13 @@ package ru.jerael.booktracker.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
