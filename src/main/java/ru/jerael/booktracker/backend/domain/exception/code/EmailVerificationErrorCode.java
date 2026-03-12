a933e0601f74f4fe4bbca425f9a85694125db551
Jerael <l.kroyan43@gmail.com>
2026-03-12 19:49:14 +0300

    feat: add tooManyRequests to EmailVerificationExceptionFactory

--- src/main/java/ru/jerael/booktracker/backend/domain/exception/code/EmailVerificationErrorCode.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/exception/code/EmailVerificationErrorCode.java
@@ -3,5 +3,6 @@ package ru.jerael.booktracker.backend.domain.exception.code;
 public enum EmailVerificationErrorCode implements ErrorCode {
     VERIFICATION_NOT_FOUND,
     TOKEN_EXPIRED,
-    INVALID_TOKEN
+    INVALID_TOKEN,
+    TOO_MANY_REQUESTS
 }
