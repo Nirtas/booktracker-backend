057777b8c12fe2a98be065f904db7a7731418cc7
Jerael <l.kroyan43@gmail.com>
2026-03-13 12:36:47 +0300

    feat: add spring security dependency and AuthTokenFilter

---
+++ src/main/java/ru/jerael/booktracker/backend/web/security/AuthTokenFilter.java
@@ -0,0 +1,40 @@
+package ru.jerael.booktracker.backend.web.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.jerael.booktracker.backend.domain.model.auth.IdentityTokenClaims;
+import ru.jerael.booktracker.backend.domain.model.auth.IdentityTokenType;
+import ru.jerael.booktracker.backend.domain.service.token.AuthTokenService;
+import java.io.IOException;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private final AuthTokenService authTokenService;
+
+    @Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            IdentityTokenClaims claims = authTokenService.authenticateToken(token, IdentityTokenType.ACCESS);
+            UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(claims.userId(), null, Collections.emptySet());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
