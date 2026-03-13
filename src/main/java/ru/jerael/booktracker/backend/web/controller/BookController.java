651fba1005bfb4f9a82840ddfeea973e78e2739d
Jerael <l.kroyan43@gmail.com>
2026-03-13 19:15:18 +0300

    feat: integrate authenticated userId into book endpoints

--- src/main/java/ru/jerael/booktracker/backend/web/controller/BookController.java
+++ src/main/java/ru/jerael/booktracker/backend/web/controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.domain.*;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.jerael.booktracker.backend.domain.constant.BookRules;
@@ -52,7 +53,7 @@ public class BookController {
 
     @Operation(summary = "Get all books")
     @GetMapping
-    public PagedModel<BookResponse> getAll(@ParameterObject Pageable pageable) {
+    public PagedModel<BookResponse> getAll(@ParameterObject Pageable pageable, @AuthenticationPrincipal UUID userId) {
         Sort.Order order = pageable.getSort().stream().findFirst().orElse(null);
         String sortBy = order != null ? order.getProperty() : PaginationRules.DEFAULT_SORT_FIELD;
         SortDirection direction = order != null && order.isAscending()
@@ -65,7 +66,6 @@ public class BookController {
             sortBy,
             direction
         );
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
         PageResult<Book> books = getBooksUseCase.execute(query, userId);
         PageResult<BookResponse> bookResponses = books.map(bookWebMapper::toResponse);
         Page<BookResponse> page = new PageImpl<>(
@@ -78,8 +78,7 @@ public class BookController {
 
     @Operation(summary = "Get book by id")
     @GetMapping("/{id}")
-    public BookResponse getById(@PathVariable UUID id) {
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
+    public BookResponse getById(@PathVariable UUID id, @AuthenticationPrincipal UUID userId) {
         Book book = getBookByIdUseCase.execute(id, userId);
         return bookWebMapper.toResponse(book);
     }
@@ -87,16 +86,18 @@ public class BookController {
     @Operation(summary = "Delete book by id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBook(@PathVariable UUID id) {
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
+    public void deleteBook(@PathVariable UUID id, @AuthenticationPrincipal UUID userId) {
         deleteBookUseCase.execute(id, userId);
     }
 
     @Operation(summary = "Update book details")
     @PatchMapping("/{id}")
-    public BookResponse updateDetails(@PathVariable UUID id, @Valid @RequestBody BookDetailsUpdateRequest request) {
+    public BookResponse updateDetails(
+        @PathVariable UUID id,
+        @Valid @RequestBody BookDetailsUpdateRequest request,
+        @AuthenticationPrincipal UUID userId
+    ) {
         BookDetailsUpdate data = bookWebMapper.toDomain(request);
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
         Book book = updateBookDetailsUseCase.execute(id, userId, data);
         return bookWebMapper.toResponse(book);
     }
@@ -104,9 +105,8 @@ public class BookController {
     @Operation(summary = "Create book")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookResponse create(@Valid @RequestBody BookCreationRequest request) {
+    public BookResponse create(@Valid @RequestBody BookCreationRequest request, @AuthenticationPrincipal UUID userId) {
         BookCreation data = bookWebMapper.toDomain(request);
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
         Book book = createBookUseCase.execute(data, userId);
         return bookWebMapper.toResponse(book);
     }
@@ -115,11 +115,11 @@ public class BookController {
     @PostMapping(value = "/{id}/cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public BookResponse uploadCover(
         @PathVariable UUID id,
-        @RequestParam(BookRules.COVER_FIELD_NAME) MultipartFile file
+        @RequestParam(BookRules.COVER_FIELD_NAME) MultipartFile file,
+        @AuthenticationPrincipal UUID userId
     ) {
         fileValidator.validate(file, BookRules.COVER_FIELD_NAME);
         UploadCover data = uploadCoverWebMapper.toDomain(file);
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
         Book book = uploadCoverUseCase.execute(id, userId, data);
         return bookWebMapper.toResponse(book);
     }
@@ -127,15 +127,13 @@ public class BookController {
     @Operation(summary = "Delete book cover")
     @DeleteMapping("/{id}/cover")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCover(@PathVariable UUID id) {
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
+    public void deleteCover(@PathVariable UUID id, @AuthenticationPrincipal UUID userId) {
         deleteCoverUseCase.execute(id, userId);
     }
 
     @Operation(summary = "Get book cover")
     @GetMapping("/{id}/cover")
-    public ResponseEntity<Resource> getCover(@PathVariable UUID id) {
-        UUID userId = UUID.fromString("2c5781ea-1bc2-4561-a83d-26106df2526e"); // TODO: REMOVE THIS
+    public ResponseEntity<Resource> getCover(@PathVariable UUID id, @AuthenticationPrincipal UUID userId) {
         ImageFile imageFile = getBookCoverUseCase.execute(id, userId);
         return ResponseEntity.ok()
             .header(HttpHeaders.CONTENT_DISPOSITION, "filename=\"" + imageFile.fileName() + "\"")
