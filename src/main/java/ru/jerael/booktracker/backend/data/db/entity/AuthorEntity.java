5418a54cd8fe353d7969e830ad01784f70656b8a
Jerael <l.kroyan43@gmail.com>
2026-03-18 10:21:55 +0300

    feat: add AuthorRules

--- src/main/java/ru/jerael/booktracker/backend/data/db/entity/AuthorEntity.java
+++ src/main/java/ru/jerael/booktracker/backend/data/db/entity/AuthorEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.jerael.booktracker.backend.data.db.constant.Tables;
+import ru.jerael.booktracker.backend.domain.constant.AuthorRules;
 import java.util.UUID;
 
 @Table(name = Tables.AUTHORS)
@@ -18,6 +19,6 @@ public class AuthorEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "full_name")
+    @Column(name = "full_name", length = AuthorRules.AUTHOR_FULL_NAME_MAX_LENGTH, nullable = false)
     private String fullName;
 }
---
+++ src/main/java/ru/jerael/booktracker/backend/domain/constant/AuthorRules.java
@@ -0,0 +1,7 @@
+package ru.jerael.booktracker.backend.domain.constant;
+
+public final class AuthorRules {
+    private AuthorRules() {}
+
+    public static final int AUTHOR_FULL_NAME_MAX_LENGTH = 500;
+}
