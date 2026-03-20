d62cbc6fce34cdd62531b7be22641440577ada2d
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-03-20 14:15:39 +0100

    refactor: remove nullable annotations in domain models (#121)

--- src/main/java/ru/jerael/booktracker/backend/domain/model/book/BookCreation.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/model/book/BookCreation.java
@@ -1,11 +1,10 @@
 package ru.jerael.booktracker.backend.domain.model.book;
 
-import jakarta.annotation.Nullable;
 import java.util.Set;
 
 public record BookCreation(
     String title,
     String author,
-    @Nullable BookStatus status,
+    BookStatus status,
     Set<Integer> genreIds
 ) {}
--- src/main/java/ru/jerael/booktracker/backend/domain/model/book/BookDetailsUpdate.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/model/book/BookDetailsUpdate.java
@@ -1,11 +1,10 @@
 package ru.jerael.booktracker.backend.domain.model.book;
 
-import jakarta.annotation.Nullable;
 import java.util.Set;
 
 public record BookDetailsUpdate(
-    @Nullable String title,
-    @Nullable String author,
-    @Nullable BookStatus status,
-    @Nullable Set<Integer> genreIds
+    String title,
+    String author,
+    BookStatus status,
+    Set<Integer> genreIds
 ) {}
