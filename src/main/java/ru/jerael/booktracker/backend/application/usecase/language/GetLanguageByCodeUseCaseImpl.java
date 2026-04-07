dbcba4861c5285fee1f582e9dbf7abf07e348852
Jerael <l.kroyan43@gmail.com>
2026-04-07 20:23:59 +0300

    feat: implement GetLanguageByCodeUseCase

---
+++ src/main/java/ru/jerael/booktracker/backend/application/usecase/language/GetLanguageByCodeUseCaseImpl.java
@@ -0,0 +1,19 @@
+package ru.jerael.booktracker.backend.application.usecase.language;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.jerael.booktracker.backend.domain.exception.factory.LanguageExceptionFactory;
+import ru.jerael.booktracker.backend.domain.model.language.Language;
+import ru.jerael.booktracker.backend.domain.repository.LanguageRepository;
+import ru.jerael.booktracker.backend.domain.usecase.language.GetLanguageByCodeUseCase;
+
+@Component
+@RequiredArgsConstructor
+public class GetLanguageByCodeUseCaseImpl implements GetLanguageByCodeUseCase {
+    private final LanguageRepository languageRepository;
+
+    @Override
+    public Language execute(String code) {
+        return languageRepository.findByCode(code).orElseThrow(() -> LanguageExceptionFactory.languageNotFound(code));
+    }
+}
