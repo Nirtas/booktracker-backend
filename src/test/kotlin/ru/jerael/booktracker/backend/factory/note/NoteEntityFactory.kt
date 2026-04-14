b1c5732e09aa7a88ae06749acd379fc350f825c3
Jerael <l.kroyan43@gmail.com>
2026-04-14 14:12:36 +0300

    test: add NoteEntityFactory

---
+++ src/test/kotlin/ru/jerael/booktracker/backend/factory/note/NoteEntityFactory.kt
@@ -0,0 +1,24 @@
+package ru.jerael.booktracker.backend.factory.note
+
+import ru.jerael.booktracker.backend.data.db.entity.BookEntity
+import ru.jerael.booktracker.backend.data.db.entity.NoteEntity
+import ru.jerael.booktracker.backend.domain.model.note.NoteType
+import java.time.Instant
+import java.util.*
+
+object NoteEntityFactory {
+    fun createNoteEntity(
+        id: UUID = UUID.randomUUID(),
+        book: BookEntity = BookEntity().apply { this.id = UUID.randomUUID() },
+        type: NoteType = NoteType.TEXT,
+        textContent: String? = "text",
+        fileName: String? = null,
+        pageNumber: Int = 50,
+        createdAt: Instant = Instant.now()
+    ): NoteEntity {
+        return NoteEntity().apply {
+            this.id = id; this.book = book; this.type = type; this.textContent = textContent
+            this.fileName = fileName; this.pageNumber = pageNumber; this.createdAt = createdAt
+        }
+    }
+}
