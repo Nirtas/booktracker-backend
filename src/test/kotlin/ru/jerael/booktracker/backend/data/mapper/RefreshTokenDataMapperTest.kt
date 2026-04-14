319cc928435497b8ea7901d4eb008b9307924216
Jerael <l.kroyan43@gmail.com>
2026-04-14 20:28:13 +0300

    test: migrate RefreshTokenDataMapperTest to Kotlin

---
+++ src/test/kotlin/ru/jerael/booktracker/backend/data/mapper/RefreshTokenDataMapperTest.kt
@@ -0,0 +1,38 @@
+package ru.jerael.booktracker.backend.data.mapper
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import ru.jerael.booktracker.backend.factory.auth.AuthDomainFactory
+import ru.jerael.booktracker.backend.factory.auth.AuthEntityFactory
+
+class RefreshTokenDataMapperTest {
+    private val mapper = RefreshTokenDataMapper()
+    
+    @Test
+    fun `toEntity should map RefreshToken to RefreshTokenEntity`() {
+        val domain = AuthDomainFactory.createRefreshToken()
+        
+        val entity = mapper.toEntity(domain)
+        
+        with(entity) {
+            assertEquals(domain.id, id)
+            assertEquals(domain.userId, user.id)
+            assertEquals(domain.tokenHash, tokenHash)
+            assertEquals(domain.expiresAt, expiresAt)
+        }
+    }
+    
+    @Test
+    fun `toDomain should map RefreshTokenEntity to RefreshToken`() {
+        val entity = AuthEntityFactory.createRefreshTokenEntity()
+        
+        val domain = mapper.toDomain(entity)
+        
+        with(domain) {
+            assertEquals(entity.id, id)
+            assertEquals(entity.user.id, userId)
+            assertEquals(entity.tokenHash, tokenHash)
+            assertEquals(entity.expiresAt, expiresAt)
+        }
+    }
+}
