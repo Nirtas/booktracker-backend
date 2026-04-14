85e854bc47ba10c644a9d8c3e67cd93e4d969229
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-04-14 13:18:58 +0200

    test: migrate NoteDataMapperTest to Kotlin (#151)
    * test: add NoteEntityFactory
    
    * test: migrate NoteDataMapperTest to Kotlin

---
+++ src/test/kotlin/ru/jerael/booktracker/backend/data/mapper/NoteDataMapperTest.kt
@@ -0,0 +1,44 @@
+package ru.jerael.booktracker.backend.data.mapper
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import ru.jerael.booktracker.backend.factory.note.NoteDomainFactory
+import ru.jerael.booktracker.backend.factory.note.NoteEntityFactory
+
+class NoteDataMapperTest {
+    private val mapper = NoteDataMapper()
+    
+    @Test
+    fun `toEntity should map Note to NoteEntity`() {
+        val note = NoteDomainFactory.createNote()
+        
+        val entity = mapper.toEntity(note)
+        
+        with(entity) {
+            assertEquals(note.id, id)
+            assertEquals(note.bookId, book.id)
+            assertEquals(note.type, type)
+            assertEquals(note.textContent, textContent)
+            assertEquals(note.fileName, fileName)
+            assertEquals(note.pageNumber, pageNumber)
+            assertEquals(note.createdAt, createdAt)
+        }
+    }
+    
+    @Test
+    fun `toDomain should map NoteEntity to Note`() {
+        val entity = NoteEntityFactory.createNoteEntity()
+        
+        val note = mapper.toDomain(entity)
+        
+        with(note) {
+            assertEquals(entity.id, id)
+            assertEquals(entity.book.id, bookId)
+            assertEquals(entity.type, type)
+            assertEquals(entity.textContent, textContent)
+            assertEquals(entity.fileName, fileName)
+            assertEquals(entity.pageNumber, pageNumber)
+            assertEquals(entity.createdAt, createdAt)
+        }
+    }
+}
