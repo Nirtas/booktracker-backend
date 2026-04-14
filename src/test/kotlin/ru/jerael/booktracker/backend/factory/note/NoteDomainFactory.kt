b5187b780b789e62ab1c283f5e41381578c52b72
Jerael <l.kroyan43@gmail.com>
2026-04-14 12:40:45 +0300

    test: add NoteDomainFactory

---
+++ src/test/kotlin/ru/jerael/booktracker/backend/factory/note/NoteDomainFactory.kt
@@ -0,0 +1,20 @@
+package ru.jerael.booktracker.backend.factory.note
+
+import ru.jerael.booktracker.backend.domain.model.note.Note
+import ru.jerael.booktracker.backend.domain.model.note.NoteType
+import java.time.Instant
+import java.util.*
+
+object NoteDomainFactory {
+    fun createNote(
+        id: UUID = UUID.randomUUID(),
+        bookId: UUID = UUID.randomUUID(),
+        type: NoteType = NoteType.TEXT,
+        textContent: String? = "text",
+        fileName: String? = null,
+        pageNumber: Int = 50,
+        createdAt: Instant = Instant.now()
+    ): Note {
+        return Note(id, bookId, type, textContent, fileName, pageNumber, createdAt)
+    }
+}
