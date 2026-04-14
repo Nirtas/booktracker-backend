a924468dd4019fe98ad0629a4f2cdc3aff41d68d
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-04-14 19:30:35 +0200

    test: migrate RefreshTokenDataMapperTest to Kotlin (#156)
    * test: add AuthEntityFactory
    
    * test: migrate RefreshTokenDataMapperTest to Kotlin

---
+++ src/test/kotlin/ru/jerael/booktracker/backend/factory/auth/AuthEntityFactory.kt
@@ -0,0 +1,19 @@
+package ru.jerael.booktracker.backend.factory.auth
+
+import ru.jerael.booktracker.backend.data.db.entity.RefreshTokenEntity
+import ru.jerael.booktracker.backend.data.db.entity.UserEntity
+import java.time.Instant
+import java.util.*
+
+object AuthEntityFactory {
+    fun createRefreshTokenEntity(
+        id: UUID = UUID.randomUUID(),
+        user: UserEntity = UserEntity().apply { this.id = UUID.randomUUID() },
+        tokenHash: String = "token hash",
+        expiresAt: Instant = Instant.now().plusSeconds(6000)
+    ): RefreshTokenEntity {
+        return RefreshTokenEntity().apply {
+            this.id = id; this.user = user; this.tokenHash = tokenHash; this.expiresAt = expiresAt
+        }
+    }
+}
