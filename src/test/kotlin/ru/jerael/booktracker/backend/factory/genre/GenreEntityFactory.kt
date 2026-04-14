d8c98a3afb856abdbf880b506d4cc72461e9c778
Jerael <l.kroyan43@gmail.com>
2026-04-14 13:57:20 +0300

    test: add GenreEntityFactory

---
+++ src/test/kotlin/ru/jerael/booktracker/backend/factory/genre/GenreEntityFactory.kt
@@ -0,0 +1,12 @@
+package ru.jerael.booktracker.backend.factory.genre
+
+import ru.jerael.booktracker.backend.data.db.entity.GenreEntity
+
+object GenreEntityFactory {
+    fun createGenreEntity(
+        id: Int = 1,
+        name: String = "Genre 1"
+    ): GenreEntity {
+        return GenreEntity().apply { this.id = id; this.name = name }
+    }
+}
