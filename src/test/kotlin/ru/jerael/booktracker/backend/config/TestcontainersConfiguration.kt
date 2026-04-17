28d039717d98153f757c8541f92ba92ba513a2f4
Vladimir Kroyan <l.kroyan43@gmail.com>
2026-04-17 20:27:00 +0300

    test: migrate MinioBookCoverStorageTest to Kotlin (#198)
    * test: add minioContainer configuration
    
    * test: migrate MinioBookCoverStorageTest to Kotlin

--- src/test/kotlin/ru/jerael/booktracker/backend/config/TestcontainersConfiguration.kt
+++ src/test/kotlin/ru/jerael/booktracker/backend/config/TestcontainersConfiguration.kt
@@ -4,6 +4,9 @@ import ch.martinelli.oss.testcontainers.mailpit.MailpitContainer
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
+import org.springframework.test.context.DynamicPropertyRegistrar
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
@@ -11,4 +14,22 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     fun mailpitContainer(): MailpitContainer = MailpitContainer()
+    
+    @Bean
+    fun minioContainer(): GenericContainer<*> {
+        return GenericContainer(DockerImageName.parse("minio/minio:RELEASE.2025-09-07T16-13-09Z"))
+            .withEnv("MINIO_ROOT_USER", "testuser")
+            .withEnv("MINIO_ROOT_PASSWORD", "testpassword")
+            .withCommand("server /data")
+            .withExposedPorts(9000)
+    }
+    
+    @Bean
+    fun minioPropertyRegistrar(minioContainer: GenericContainer<*>): DynamicPropertyRegistrar {
+        return DynamicPropertyRegistrar { registry ->
+            registry.add("app.minio.url") {
+                "http://" + minioContainer.host + ":" + minioContainer.getMappedPort(9000)
+            }
+        }
+    }
 }
