7fb82d3e0e71c2d791a0e14d56c2dcdba4549240
Jerael <l.kroyan43@gmail.com>
2026-04-23 00:36:53 +0300

    refactor: add single ValidationError constructor

--- src/main/java/ru/jerael/booktracker/backend/domain/exception/factory/FileValidationExceptionFactory.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/exception/factory/FileValidationExceptionFactory.java
@@ -3,33 +3,40 @@ package ru.jerael.booktracker.backend.domain.exception.factory;
 import ru.jerael.booktracker.backend.domain.constant.ImageRules;
 import ru.jerael.booktracker.backend.domain.exception.ValidationException;
 import ru.jerael.booktracker.backend.domain.exception.code.FileValidationErrorCode;
+import ru.jerael.booktracker.backend.domain.exception.model.ValidationError;
 import java.util.Map;
 
 public class FileValidationExceptionFactory {
     public static ValidationException emptyFileName(String field) {
         return new ValidationException(
-            FileValidationErrorCode.EMPTY_FILE_NAME,
-            "File name cannot be empty",
-            field,
-            Map.of()
+            new ValidationError(
+                FileValidationErrorCode.EMPTY_FILE_NAME.name(),
+                field,
+                "File name cannot be empty",
+                Map.of()
+            )
         );
     }
 
     public static ValidationException emptyFileContent(String field) {
         return new ValidationException(
-            FileValidationErrorCode.EMPTY_FILE_CONTENT,
-            "File content cannot be empty",
-            field,
-            Map.of()
+            new ValidationError(
+                FileValidationErrorCode.EMPTY_FILE_CONTENT.name(),
+                field,
+                "File content cannot be empty",
+                Map.of()
+            )
         );
     }
 
     public static ValidationException unsupportedFileContentType(String contentType, String field) {
         return new ValidationException(
-            FileValidationErrorCode.UNSUPPORTED_FILE_CONTENT_TYPE,
-            "File content type " + contentType + " is not supported",
-            field,
-            Map.of("allowed", ImageRules.ALLOWED_MIME_TYPES)
+            new ValidationError(
+                FileValidationErrorCode.UNSUPPORTED_FILE_CONTENT_TYPE.name(),
+                field,
+                "File content type " + contentType + " is not supported",
+                Map.of("allowed", ImageRules.ALLOWED_MIME_TYPES)
+            )
         );
     }
 }
--- src/test/kotlin/ru/jerael/booktracker/backend/web/validator/FileValidatorTest.kt
+++ src/test/kotlin/ru/jerael/booktracker/backend/web/validator/FileValidatorTest.kt
@@ -1,11 +1,10 @@
 package ru.jerael.booktracker.backend.web.validator
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import ru.jerael.booktracker.backend.domain.exception.ValidationException
-import ru.jerael.booktracker.backend.domain.exception.code.FileValidationErrorCode
 import ru.jerael.booktracker.backend.factory.file.FileFactory
 
 class FileValidatorTest {
@@ -22,43 +21,43 @@ class FileValidatorTest {
     fun `when file is null, validate should throw ValidationException`() {
         val file = null
         
-        val errorCode = assertThrows(ValidationException::class.java) {
+        val exception = assertThrows(ValidationException::class.java) {
             validator.validate(file, "cover")
-        }.errorCode
+        }
         
-        assertEquals(FileValidationErrorCode.EMPTY_FILE_CONTENT, errorCode)
+        assertThat(exception.errors).anyMatch { it.field.equals("cover") }
     }
     
     @Test
     fun `when file is empty, validate should throw ValidationException`() {
         val file = FileFactory.createMockMultipartFile(content = ByteArray(0))
         
-        val errorCode = assertThrows(ValidationException::class.java) {
+        val exception = assertThrows(ValidationException::class.java) {
             validator.validate(file, "cover")
-        }.errorCode
+        }
         
-        assertEquals(FileValidationErrorCode.EMPTY_FILE_CONTENT, errorCode)
+        assertThat(exception.errors).anyMatch { it.field.equals("cover") }
     }
     
     @Test
     fun `when fileName is empty, validate should throw ValidationException`() {
         val file = FileFactory.createMockMultipartFile(originalFileName = "")
         
-        val errorCode = assertThrows(ValidationException::class.java) {
+        val exception = assertThrows(ValidationException::class.java) {
             validator.validate(file, "cover")
-        }.errorCode
+        }
         
-        assertEquals(FileValidationErrorCode.EMPTY_FILE_NAME, errorCode)
+        assertThat(exception.errors).anyMatch { it.field.equals("cover") }
     }
     
     @Test
     fun `when fileName is blank, validate should throw ValidationException`() {
         val file = FileFactory.createMockMultipartFile(originalFileName = "  ")
         
-        val errorCode = assertThrows(ValidationException::class.java) {
+        val exception = assertThrows(ValidationException::class.java) {
             validator.validate(file, "cover")
-        }.errorCode
+        }
         
-        assertEquals(FileValidationErrorCode.EMPTY_FILE_NAME, errorCode)
+        assertThat(exception.errors).anyMatch { it.field.equals("cover") }
     }
 }
