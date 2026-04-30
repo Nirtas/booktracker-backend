a8beae8adfbff39abcb9857224dddf95ea289e5b
Jerael <l.kroyan43@gmail.com>
2026-04-30 09:34:04 +0300

    feat: add toResponses mapper to AuthorWebMapper

--- src/main/java/ru/jerael/booktracker/backend/web/mapper/AuthorWebMapper.java
+++ src/main/java/ru/jerael/booktracker/backend/web/mapper/AuthorWebMapper.java
@@ -3,6 +3,8 @@ package ru.jerael.booktracker.backend.web.mapper;
 import ru.jerael.booktracker.backend.domain.model.author.Author;
 import ru.jerael.booktracker.backend.web.annotation.WebMapper;
 import ru.jerael.booktracker.backend.web.dto.author.AuthorResponse;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @WebMapper
 public class AuthorWebMapper {
@@ -13,5 +15,11 @@ public class AuthorWebMapper {
             author.id(),
             author.fullName()
         );
+    }
+
+    public Set<AuthorResponse> toResponses(Set<Author> authors) {
+        if (authors == null) return Set.of();
+
+        return authors.stream().map(this::toResponse).collect(Collectors.toSet());
     }
 }
