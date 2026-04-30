dc579cc80478b0fbe0d01794d7bde9cae542e640
Jerael <l.kroyan43@gmail.com>
2026-04-30 10:36:40 +0300

    feat: add findAllByNames to GenreRepository

--- src/main/java/ru/jerael/booktracker/backend/data/db/repository/JpaGenreRepository.java
+++ src/main/java/ru/jerael/booktracker/backend/data/db/repository/JpaGenreRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.jerael.booktracker.backend.data.db.entity.GenreEntity;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface JpaGenreRepository extends JpaRepository<GenreEntity, Integer> {
     List<GenreEntity> findAllByOrderByNameAsc();
+
+    List<GenreEntity> findAllByNameInIgnoreCase(Set<String> names);
 }
