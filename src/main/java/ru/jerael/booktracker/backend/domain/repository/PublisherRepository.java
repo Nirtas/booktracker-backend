d48b9b01672bdad7613d067be64cd7455c1f7d09
Jerael <l.kroyan43@gmail.com>
2026-05-06 15:50:30 +0300

    feat: implement search by name in PublisherRepositoryImpl

--- src/main/java/ru/jerael/booktracker/backend/domain/repository/PublisherRepository.java
+++ src/main/java/ru/jerael/booktracker/backend/domain/repository/PublisherRepository.java
@@ -1,5 +1,7 @@
 package ru.jerael.booktracker.backend.domain.repository;
 
+import ru.jerael.booktracker.backend.domain.model.pagination.PageQuery;
+import ru.jerael.booktracker.backend.domain.model.pagination.PageResult;
 import ru.jerael.booktracker.backend.domain.model.publisher.Publisher;
 import java.util.Optional;
 
@@ -7,4 +9,6 @@ public interface PublisherRepository {
     Optional<Publisher> findByName(String name);
 
     Publisher save(Publisher publisher);
+
+    PageResult<Publisher> searchByName(PageQuery pageQuery, String query);
 }
