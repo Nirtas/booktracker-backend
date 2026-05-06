eb7b525d4441be38333e89a9fc9e81ca0dc7c29b
Jerael <l.kroyan43@gmail.com>
2026-05-06 15:36:32 +0300

    feat: implement search by full name in AuthorRepositoryImpl

--- src/main/java/ru/jerael/booktracker/backend/data/repository/AuthorRepositoryImpl.java
+++ src/main/java/ru/jerael/booktracker/backend/data/repository/AuthorRepositoryImpl.java
@@ -1,12 +1,17 @@
 package ru.jerael.booktracker.backend.data.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import ru.jerael.booktracker.backend.data.db.entity.AuthorEntity;
 import ru.jerael.booktracker.backend.data.db.repository.JpaAuthorRepository;
 import ru.jerael.booktracker.backend.data.mapper.AuthorDataMapper;
 import ru.jerael.booktracker.backend.domain.model.author.Author;
+import ru.jerael.booktracker.backend.domain.model.pagination.PageQuery;
+import ru.jerael.booktracker.backend.domain.model.pagination.PageResult;
 import ru.jerael.booktracker.backend.domain.repository.AuthorRepository;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -34,5 +39,21 @@ public class AuthorRepositoryImpl implements AuthorRepository {
         return jpaAuthorRepository.findAllByFullNameInIgnoreCase(names).stream()
             .map(authorDataMapper::toDomain)
             .collect(Collectors.toSet());
+    }
+
+    @Override
+    public PageResult<Author> searchByFullName(PageQuery pageQuery, String query) {
+        int page = pageQuery != null ? pageQuery.page() : 0;
+        int size = pageQuery != null ? pageQuery.size() : 5;
+        Pageable pageable = PageRequest.of(page, size);
+
+        List<AuthorEntity> entities = jpaAuthorRepository.searchBySimilarity(query, pageable);
+        return new PageResult<>(
+            entities.stream().map(authorDataMapper::toDomain).toList(),
+            size,
+            page,
+            entities.size(),
+            1
+        );
     }
 }
