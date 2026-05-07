9950cbf2afe4f49b1bda03672505a7d8e4af5340
Jerael <l.kroyan43@gmail.com>
2026-05-07 12:40:10 +0300

    feat: implement PublisherController with autocomplete endpoint

--- src/main/java/ru/jerael/booktracker/backend/web/mapper/PublisherWebMapper.java
+++ src/main/java/ru/jerael/booktracker/backend/web/mapper/PublisherWebMapper.java
@@ -1,8 +1,10 @@
 package ru.jerael.booktracker.backend.web.mapper;
 
+import ru.jerael.booktracker.backend.domain.model.pagination.PageResult;
 import ru.jerael.booktracker.backend.domain.model.publisher.Publisher;
 import ru.jerael.booktracker.backend.web.annotation.WebMapper;
 import ru.jerael.booktracker.backend.web.dto.publisher.PublisherResponse;
+import java.util.List;
 
 @WebMapper
 public class PublisherWebMapper {
@@ -13,5 +15,11 @@ public class PublisherWebMapper {
             publisher.id(),
             publisher.name()
         );
+    }
+
+    public List<PublisherResponse> toResponses(PageResult<Publisher> publishers) {
+        if (publishers == null) return List.of();
+
+        return publishers.content().stream().map(this::toResponse).toList();
     }
 }
